e383d82fd9d33e883e17589130be4de9aaa7f4fa
geonil2 <kji8922@naver.com>
2022-01-04 20:31:09 +0900

    add item(note, todo, video)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VvideoComponent } from "./components/page/item/video.js";
+import { PageComponent } from "./components/page/page.js";
 
 class App {
     private readonly page: PageComponent;
@@ -6,6 +10,18 @@ class App {
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
+
+        const video = new VvideoComponent('Video Title', 'https://www.youtube.com/watch?v=HTewyS8hq0c');
+        video.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent('Note Title', 'Note Body');
+        note.attachTo(appRoot, 'beforeend');
+
+        const todo = new TodoComponent('Todo Title', 'Todo Item');
+        todo.attachTo(appRoot, 'beforeend');
     };
 }
 
